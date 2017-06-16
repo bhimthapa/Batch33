9ee16e85c70a0d4540cdc31bb0767a81ae8f1a5c
ramesh <ramesh@RAMESH-PC>
2017-06-15 22:13:10 -0400

    Day to Crud Update.

--- Week1.CRUD.App.Console/Program.cs
+++ Week1.CRUD.App.Console/Program.cs
@@ -14,55 +14,76 @@ namespace Week1.CRUD.App.Console_App
     {
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("1 for Display All");
-            Console.WriteLine("2 for Add New Record");
-            Console.WriteLine("3 for Update");
-            Console.WriteLine("4 for Delete");
-            Console.WriteLine("5 for Display By Id");
-            Console.WriteLine("6 for Exit");
-            Console.WriteLine("Enter your Choice....");
-            string choice = Console.ReadLine();
-
-            switch(choice)
+            while (true)
             {
-                case "1":
 
-                    CustomerDataAcess cusData = new CustomerDataAcess();
-                    SqlDataReader rd = cusData.GetCustomerData();
-                    while(rd.Read())
-                    {
-                        Console.WriteLine("**********************************************");
-                        Console.WriteLine("Id :{0}", rd.GetInt32(0));
-                        Console.WriteLine("First Name :{0}", rd.GetString(1));
-                        Console.WriteLine("Last Name :{0}", rd.GetString(2));
-                        Console.WriteLine("Email :{0}", rd.GetString(3));
-                        Console.WriteLine("Phone Number :{0}", rd.GetString(4));
-                        Console.WriteLine("**********************************************");
-                    }
+                Console.WriteLine("1 for Display All");
+                Console.WriteLine("2 for Add New Record");
+                Console.WriteLine("3 for Update");
+                Console.WriteLine("4 for Delete");
+                Console.WriteLine("5 for Display By Id");
+                Console.WriteLine("6 for Exit");
+                Console.WriteLine("Enter your Choice....");
+                string choice = Console.ReadLine();
+                CustomerDataAcess cusData = new CustomerDataAcess();
+                switch (choice)
+                {
+                    case "1":
 
 
-                    break;
+                        SqlDataReader rd = cusData.GetCustomerData();
+                        while (rd.Read())
+                        {
+                            Console.WriteLine("**********************************************");
+                            Console.WriteLine("Id :{0}", rd.GetInt32(0));
+                            Console.WriteLine("First Name :{0}", rd.GetString(1));
+                            Console.WriteLine("Last Name :{0}", rd.GetString(2));
+                            Console.WriteLine("Email :{0}", rd.GetString(3));
+                            Console.WriteLine("Phone Number :{0}", rd.GetString(4));
+                            Console.WriteLine("**********************************************");
+                        }
 
-                case "2":
-                    Console.WriteLine("Case 2 selected");
-                    break;
-                case "3":
-                    Console.WriteLine("Case 3 selected");
-                    break;
-                case "4":
-                    Console.WriteLine("Case 4 selected");
-                    break;
-                case "5":
-                    Console.WriteLine("Case 5 selected");
-                    break;
-                case "6":
-                    Console.WriteLine("Case 6 selected");
-                    break;
-            }
 
-            Console.Read();
+                        break;
 
+                    case "2":
+                        Console.WriteLine("Enter First Name");
+                        string fn = Console.ReadLine();
+                        Console.WriteLine("Enter Last Name");
+                        string ln = Console.ReadLine();
+                        Console.WriteLine("Enter Email");
+                        string em = Console.ReadLine();
+                        Console.WriteLine("Enter Phone Number");
+                        string ph = Console.ReadLine();
+                        cusData.AddNewCustomer(fn, ln, em, ph);
+                        break;
+                    case "3":
+                        Console.WriteLine("Enter Id to Update");
+                        string id = Console.ReadLine();
+                        Console.WriteLine("Enter First Name");
+                        string ufn = Console.ReadLine();
+                        Console.WriteLine("Enter Last Name");
+                        string uln = Console.ReadLine();
+                        Console.WriteLine("Enter Email");
+                        string uem = Console.ReadLine();
+                        Console.WriteLine("Enter Phone Number");
+                        string uph = Console.ReadLine();
+                        cusData.UpdateNewCustomer(ufn, uln, uem, uph, Convert.ToInt32(id));
+                        break;
+                    case "4":
+                        Console.WriteLine("Enter Id to Delete");
+                        string did = Console.ReadLine();
+                        cusData.DeleteCustomer(Convert.ToInt32(did));
+                        break;
+                    case "5":
+                        Console.WriteLine("Case 5 selected");
+                        break;
+                    case "6":
+                        Environment.Exit(0);
+                        break;
+                }
+
+                     }
         }
     }
 }
--- Week1.CRUD.App.Data/CustomerDataAcess.cs
+++ Week1.CRUD.App.Data/CustomerDataAcess.cs
@@ -19,13 +19,76 @@ namespace Week1.CRUD.App.Data
             SqlConnection conn = new SqlConnection("Data Source=.;Initial Catalog=Batch33;Integrated Security=True");
             //Step 4: Invoke storedprocedure
             SqlCommand cmd = new SqlCommand("GetCustomers", conn);
-
+            
             conn.Open();
             //Step 5: Read database table data
             SqlDataReader rd = cmd.ExecuteReader();
             return rd;
+            
+        }
+
+        public void AddNewCustomer(string fn,string ln,string email, string phone)
+        {
+            // Step  3: Configure connection string
+            SqlConnection conn = new SqlConnection("Data Source=.;Initial Catalog=Batch33;Integrated Security=True");
+            //Step 4: Invoke storedprocedure
+            SqlCommand cmd = new SqlCommand("AddNewCustomer", conn);
+
+            conn.Open();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("@FirstName", SqlDbType.VarChar, 50).Value = fn;
+            cmd.Parameters.Add("@LastName", SqlDbType.VarChar, 50).Value = ln;
+            cmd.Parameters.Add("@Email", SqlDbType.VarChar, 50).Value = email;
+            cmd.Parameters.Add("@Phone", SqlDbType.VarChar, 12).Value = phone;
+
+            cmd.ExecuteNonQuery();
+ 
+
 
         }
+
+        public void UpdateNewCustomer(string fn, string ln, string email, string phone, int id)
+        {
+            // Step  3: Configure connection string
+            SqlConnection conn = new SqlConnection("Data Source=.;Initial Catalog=Batch33;Integrated Security=True");
+            //Step 4: Invoke storedprocedure
+            SqlCommand cmd = new SqlCommand("UpdateNewCustomer", conn);
+
+            conn.Open();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("@FirstName", SqlDbType.VarChar, 50).Value = fn;
+            cmd.Parameters.Add("@LastName", SqlDbType.VarChar, 50).Value = ln;
+            cmd.Parameters.Add("@Email", SqlDbType.VarChar, 50).Value = email;
+            cmd.Parameters.Add("@Phone", SqlDbType.VarChar, 12).Value = phone;
+            cmd.Parameters.Add("@Id", SqlDbType.Int).Value = id;
+
+            cmd.ExecuteNonQuery();
+
+
+
+        }
+
+
+        public void DeleteCustomer(int id)
+        {
+            // Step  3: Configure connection string
+            SqlConnection conn = new SqlConnection("Data Source=.;Initial Catalog=Batch33;Integrated Security=True");
+            //Step 4: Invoke storedprocedure
+            SqlCommand cmd = new SqlCommand("DeleteCustomer", conn);
+
+            conn.Open();
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.Add("@Id", SqlDbType.Int).Value = id;
+
+            cmd.ExecuteNonQuery();
+
+
+
+        }
+
+
+
 
     }
 }
